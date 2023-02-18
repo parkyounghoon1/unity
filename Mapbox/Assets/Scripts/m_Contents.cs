b602939ea50dd027eb93dd1337c72a4f444d1371
Park Younghoon <68407368+see4894@users.noreply.github.com>
2023-02-18 20:57:31 +0900

    a

---
+++ Mapbox/Assets/Scripts/m_Contents.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class m_Contents
+{
+    public static string SCENE_WORLD = "World"; // 메인화면
+    public static string SCENE_CAPTURE = "Capture"; //미션
+
+    public static string TAG_Monster = "Monster"; //몬스터
+    public static string TAG_Orb = "Orb";
+}
